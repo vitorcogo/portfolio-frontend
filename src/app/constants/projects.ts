4550da4cabbcc68396828013a55dbe43e791c0f3
Vitor Cogo <vitor_cogo@outlook.com>
2024-11-04 22:36:49 -0300

    spotify profile project

--- src/app/constants/projects.ts
+++ src/app/constants/projects.ts
@@ -2,6 +2,14 @@ import { Project } from "../models/project.interface";
 
 export const Projects: Project[] = [
   {
+    title: 'Perfil Spotify',
+    description: 'Aplicação que permite visualizar informações detalhadas sobre sua conta do Spotify, incluindo suas músicas e artistas mais ouvidos, além das músicas recentemente ouvidas. A interface foi projetada para proporcionar uma forma simples e intuitiva de explorar esses dados, destacando as preferências musicais de forma clara e acessível.',
+    imgUrl: './../../assets/spotify_profile.jpg',
+    repositoryUrl: 'https://github.com/vitorcogo/spotify-profile',
+    deployUrl: 'https://spotify-profile-vitorcogos-projects.vercel.app/',
+    technologies: ['Angular 17', 'SCSS', 'Git', 'Typesscript', 'RxJs', 'Figma', 'Spotify API']
+  },
+  {
     title: 'Portfólio',
     description: 'Portfólio com foco na minha experiência como desenvolvedor Front-End. Desenvolvido com Angular 16.',
     imgUrl: './../../assets/protfolio_frontend.jpg',
@@ -15,5 +23,5 @@ export const Projects: Project[] = [
     imgUrl: './../../assets/vittorio_veneto_home.jpeg',
     technologies: ['Angular 11', 'SCSS', 'Git', 'Typescript', 'RxJs', 'Node.js'],
     repositoryUrl: 'https://github.com/vitorcogo/vittorio-veneto-front'
-  }
+  },
 ]
